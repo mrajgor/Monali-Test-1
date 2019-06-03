402ec412cbc0be214e6ee99fb5dc0063e349faa4
Metin Yarci <metin.yarci@aurea.com>
2019-06-03 09:08:24 +0300

    service added

---
+++ src/main/java/hello/GreetingService.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingService {
+
+    private static final String CLASS = "class";
+
+    public void methodRevision1() {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
