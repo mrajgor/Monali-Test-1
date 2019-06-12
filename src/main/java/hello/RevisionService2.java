aac1650f8dda53720ade63310671613973bc835e
Monali Rajgor <monali.rajgor@aurea.com>
2019-06-12 19:59:00 +0530

    New RevisionService 2 File

---
+++ src/main/java/hello/RevisionService2.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RevisionService2 {
+
+    private static final String CLASS = "class";
+
+    public void methodRevision2() {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
