a9dde1ac83afca860c940062f46b368fdd445fc5
Monali Rajgor <monali.rajgor@aurea.com>
2019-06-13 18:08:51 +0530

    Revision Controller

---
+++ src/main/java/hello/RevisionController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RevisionController {
+
+    private static final String CLASS = "class";
+
+    public void methodRevisionCpntroller1() {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
